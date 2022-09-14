bb74374eac4fa287e4a5c2ca60361958c54d6be6
Juan Jose Martinez <josemargri3@gmail.com>
2022-09-14 00:11:33 -0500

    Agregamos una vista unica para cada pagina y las consultas necesarias para acceder a su informacion

--- ProyectoCine/films/urls.py
+++ ProyectoCine/films/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("peliculas/",views.show,name="peliculas")
+    path("peliculas/",views.show,name="peliculas"),
+    path("peliculas/<str:name>",views.one_film,name="pelicula")
 ]
--- ProyectoCine/films/views.py
+++ ProyectoCine/films/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import age,film,gener
 # Create your views here.
 
 def show(request):
 
-    peliculas = film.objects.all()
+    peliculas = film.objects.all()  
 
     nueva_lista = [peliculas[i:i+4] for i in range(0, len(peliculas), 4)]
     
@@ -13,3 +13,19 @@ def show(request):
         "peliculas":nueva_lista,
         "title": "Peliculas"
     })
+
+def one_film(request,name):
+    try:
+        pelicula = film.objects.get(name=name)
+        id = pelicula.id
+        genero = film.genero.through.objects.filter(film_id=id).values_list('gener_id')
+        genero = gener.objects.filter(id__in=genero).values_list('name',flat=True)
+    
+        return render(request,'showonefilm.html',{
+            "genero":genero,
+            "pelicula":pelicula
+        })
+    except:
+        return redirect('/peliculas/')
+
+    
